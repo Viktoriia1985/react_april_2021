a943f90a59bf4866f5bf5c2dc4e37054c5b44037
Viktoriia Krat <flightsspb@gmail.com>
2021-06-24 12:33:07 +0300

    complete lesson_1 part_2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
- import {useState, useEffect} from 'react';
+import './App.css'
+import Users from "./components/users/Users";
 
-  function App() {
+export default function App() {
 
-     return (
-         <div>
+    return (
+        <div>
+
+            <Users/>
+
+        </div>
+    );
+}
 
 
-         </div>
-     );
- }
- export default App;
 
 
---
+++ src/components/users/Users.js
@@ -0,0 +1,29 @@
+
+import {useState, useEffect} from 'react';
+export default function Users() {
+
+
+    let [usersList, setUsersList] = useState([]);
+
+
+      useEffect(() => {
+
+          fetch('https://jsonplaceholder.typicode.com/users')
+              .then(value => value.json())
+              .then(response=> {
+                  console.log(response);
+                  setUsersList(response);
+              });
+
+      }, []);
+
+    return (
+        <div>
+            {
+                usersList
+                    .map(value => <div>{value.id} - {value.name}</div>)
+            }
+        </div>
+    );
+}
+
