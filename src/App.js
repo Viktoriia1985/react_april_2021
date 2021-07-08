7071e1a876f0d0fe746af53257d02ae54f4729b9
Viktoriia Krat <flightsspb@gmail.com>
2021-07-08 14:19:43 +0300

    redux homework_1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,81 @@
- export default function App() {
-     
-     return (
-         <div>
-             
-             App
-             
-         </div>
-     );
- }
- 
- 
+import logo from './logo.svg';
+import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+
+// basic concepts (flux, action, dispatch, reducer, context)
+// counter example
+// fetch data from json placeholder (last priority)
+
+const SomeNestedChildComponent = () => {
+    const counter = useSelector(({counterValue}) => counterValue);
+    console.log(counter, 'new result')
+
+    // const dispatch = useDispatch();
+
+    return (
+        <header className="App-header">
+            <h1>{counter}</h1>
+            {/*<button onClick={() => {*/}
+            {/*    dispatch({type: 'INC'})*/}
+            {/*}}>inc</button>*/}
+
+
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    )
+}
+
+const SomeChildComponent = () => {
+    return (
+        <SomeNestedChildComponent/>
+    )
+}
+
+export default function App() {
+    const dispatch = useDispatch();
+
+    return (
+        <div className='App'>
+            <div>
+                <button onClick={() => {
+                    dispatch({type: 'INC'})
+                }}>inc
+                </button>
+            </div>
+
+            <div>
+                <button onClick={() => {
+                    dispatch({type: 'DEC'})
+                }}>dec
+                </button>
+
+            </div>
+
+            <div>
+                <button onClick={() => {
+                    dispatch({type: 'RESET'})
+                }}>reset
+                </button>
+            </div>
+
+            <div>
+                <button onClick={() => {
+                    dispatch({type: 'INC_CUSTOM', payload: 123})
+                }}>custom
+                </button>
+            </div>
+            <SomeChildComponent/>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,67 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from "redux";
+import {Provider} from 'react-redux';
+
+const initialState = {
+    counterValue: 0
+};
+const counterReducer = (state = initialState, action, payload) => {
+    // console.log({action, state})
+    console.log({payload})
+
+    switch (action.type) {
+        case 'INC': {
+            return {...state, counterValue: state.counterValue + 1};
+        }
+        case 'DEC': {
+            return {...state, counterValue: state.counterValue - 1};
+        }
+        case 'RESET': {
+            return {...state, counterValue: 0};
+        }
+        case 'INC_CUSTOM': {
+            return {...state, posts: action.payload};
+        }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(counterReducer);
+// console.log(store);
+//
+// console.log('not from subscribe:', store.getState())
+//
+// store.subscribe(() => {
+//     console.log('from subscribe:', store.getState())
+// })
+//
+// store.dispatch({
+//     type: 'INC'
+// })
+//
+// store.dispatch({
+//     type: 'INC'
+// })
+//
+// store.dispatch({
+//     type: 'INC'
+// })
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
